78e53ca9115e1f8c910f03038f4dd14f2125f842
Jie Yu <jieyu1233@gmail.com>
2024-12-19 15:18:24 -0500

    basic JavaScript rele2

--- JS/lesson3/index.js
+++ JS/lesson3/index.js
@@ -179,3 +179,163 @@
 //     userName = window.prompt(`Enter your name`);
 // }
 // console.log(`hello ${userName}`);
+
+
+//for loop
+// for(let i = 2; i <= 10; i+=2){
+//     console.log(i);
+// }
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 == 0){
+//         continue;
+//     }else if(i == 13){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//     } 
+// }
+
+
+
+
+//Number guessing game
+// const minNum = 1;
+// const maxNum = 100;
+// const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+// let attempts = 0;
+// let guess;
+// let running = true;
+// while(running){
+//     guess = Number(window.prompt(`Guess a number between ${minNum} - ${maxNum}`));
+//     // console.log(typeof guess, guess);
+//     if(isNaN(guess)){
+//         window.alert("please enter a valid number");
+//     }else if(guess < minNum || guess > maxNum){
+//         window.alert(`please enter a number between ${minNum} - ${maxNum}`);
+//     }else{
+//         attempts++;
+//         if(guess < answer){
+//             window.alert("too low! try again");
+//         }else if(guess > answer){
+//             window.alert("too high! try again");
+//         }else{
+//             window.alert(`correct! the answer is ${answer}, it took you ${attempts} attempts`);
+//             running = false;
+//         }
+//     }  
+// }
+
+
+
+//function = section of reusable code
+// let userName = window.prompt(`Enter your name`);
+// function greeting(){
+//     console.log(`hello, ${userName}`);
+// }
+// greeting();
+
+
+
+
+//variable scope = where a variable is recognized and accessible (local VS global)
+// function2();
+// function function1(){
+//     let x = 1;
+//     console.log(x);
+// }
+// function function2(){
+//     // let x = 2;
+//     console.log(x);
+// }
+
+
+
+//temperature conversion
+// const textBox = document.getElementById("textBox");
+// const toFahrenheit = document.getElementById("toFahrenheit")
+// const toCelsius = document.getElementById("toCelsius")
+// const result = document.getElementById("result")
+// let temp;
+// function convert(){
+//     temp = Number(textBox.value);
+//     if(toFahrenheit.checked){
+//         // result.textContent = "You selected to Fahrenheit";
+//         temp = temp * 9 / 5 + 32;
+//         result.textContent = temp + "°F";
+//     }else if(toCelsius.checked){
+//         // result.textContent = "You selected to =Celsius";    
+//         temp = (temp - 32) * (5 / 9);
+//         result.textContent = temp + "°C"
+//     }else{
+//         result.textContent = "Select a unit";
+//     }
+// }
+
+
+
+
+//array
+// let fruits = ["apple", "banana", "orange"];
+// fruits.push("pineapple");
+// fruits.pop();
+// fruits.unshift("mango");
+// fruits.shift();
+// let numOfFruits = fruits.length;
+// let index = fruits.indexOf("apple");
+// console.log(fruits);
+// console.log(numOfFruits);
+// for(let fruit of fruits){
+//     console.log(fruits[i]);
+// }
+// fruits.sort();
+// fruits.sort().reverse();
+
+
+
+//spread operator = ... 
+// let numbers = [1,2,3,4,5];
+// let maximum = Math.max(...numbers);
+// let minmum = Math.min(...numbers);
+// console.log(numbers);
+// console.log(maximum);
+// let username = "Bro Code";
+// let letters = [...username].join("-");
+// console.log(letters);
+// let fruits = ["apple", "orange", "banana"];
+// let vegetables = ["carrots", "potato", "tomato"]
+// let foods = [...fruits, ...vegetables, "eggs", "milk"];
+// console.log(foods);
+
+
+//rest parameters = (...rest)
+// function openFridge(...foods){
+//      console.log(foods);
+// }
+// function getFood(...foods){
+//     return foods;
+// }
+// const food1 = "pizza";
+// const food2 = "hamburger";
+// const food3 = "noodles";
+// const food4 = "sushi";
+// openFridge(food1, food2, food3, food4);
+// const foods = getFood(food1, food2, food3, food4);
+// console.log(foods);
+
+
+//dice roller
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) +1;
+        values.push(value);
+        images.push(`<img src="dice_images/dice${value}.png" alt="Dice ${value}">`);
+    }
+    diceResult.textContent = `dice: ${values.join(`, `)}`;
+    diceImages.innerHTML = images.join(``);
+}
